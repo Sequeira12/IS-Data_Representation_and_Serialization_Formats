986ab018798e516e56eb33ba11cfc981700e76df
Sequeira12 <brunomsequeira@hotmail.com>
2023-09-28 17:14:17 +0100

    Marshalling-UnMarshalling XML

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
@@ -13,12 +15,12 @@ public class Main {
         uni.setNome("Universidade de Coimbra");
         uni.setAnoInstituicao(1754);
         ArrayList<Professor> profs = new ArrayList<>();
-        for(int i = 0; i < num;i++){
+        for(int i = 1; i <= num;i++){
             Professor prof = new Professor();
             prof.setNome("prof"+i);
             prof.setIdade(60);
             ArrayList<Aluno> alunos = new ArrayList<>();
-            for(int j = 0; j < 20; j++){
+            for(int j = 1; j <= 20; j++){
                 Aluno al = new Aluno();
                 al.setIdade(18);
                 al.setNome("Aluno"+j);
@@ -32,18 +34,45 @@ public class Main {
         uni.setProfessores(profs);
         return uni;
     }
-    public static void main(String[] args) throws JAXBException, FileNotFoundException {
-        Universidade a = Criacao(1000);
-        Aluno b = new Aluno("sad",12,12);
-
+    public static void MarshallingXML(String file, Universidade uni) throws JAXBException, FileNotFoundException {
         JAXBContext contextObj = JAXBContext.newInstance(Universidade.class);
 
         Marshaller marshallerObj = contextObj.createMarshaller();
         marshallerObj.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
+        FileOutputStream s = new FileOutputStream(file);
+        marshallerObj.marshal(uni,s);
 
-        FileOutputStream s = new FileOutputStream("universidade.xml");
-        marshallerObj.marshal(a,s);
+    }
+
+    public static void UnMarshallingXML(String filename) throws JAXBException, FileNotFoundException {
+        File file = new File(filename);
+        JAXBContext jaxbContext = JAXBContext.newInstance(Universidade.class);
+
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+        Universidade e=(Universidade) jaxbUnmarshaller.unmarshal(file);
+
+
+        System.out.println(e.getNome()+ "  " + e.getAnoInstituicao());
+        ArrayList<Professor> professors = e.getProfessores();
+        for(int i = 0; i < professors.size();i++) {
+            System.out.println(professors.get(i).getNome()+ "  " + professors.get(i).getIdade()+ "  " + professors.get(i).getAnoDoutorado() );
+            ArrayList<Aluno> alun = professors.get(i).getAlunos();
+            for(int k = 0; k < alun.size();k++){
+                System.out.println(alun.get(k).getNome()+ "  " + alun.get(k).getIdade()+ "  " + alun.get(k).getAvaliacao());
+            }
+        }
+
+    }
+    public static void main(String[] args) throws JAXBException, FileNotFoundException {
+        Universidade a = Criacao(10);
+        String FileName = "universidade.xml";
+        MarshallingXML(FileName,a);
+
+
+
+        UnMarshallingXML(FileName);
 
 
 
